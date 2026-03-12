db69dbf7216441ce38a99b891b9b7f13bb43a1e9
car-search <carsearchingsam@gmail.com>
2026-03-11 22:10:11 -0700

    wtf
    d

--- src/deco/lib/decorators.py
+++ src/deco/lib/decorators.py
@@ -7,14 +7,9 @@ logger = logging.getLogger("my_sdk")
 
 R = TypeVar("R")
 
-def _create_decorator() -> Callable[[Callable[..., R]], Callable[..., R]]:
-    def decorator(func: Callable[..., R]) -> Callable[..., R]:
-        @functools.wraps(func)
-        def wrapper(*args: Any, **kwargs: Any) -> R:
-            # Using the logger instead of print
-            logger.info("Executing function: %s", func.__name__)
-            return func(*args, **kwargs)
-        return wrapper
-    return decorator
-
-workflow = _create_decorator()
+def workflow(func: Callable[..., R]) -> Callable[..., R]:
+    @functools.wraps(func)
+    def wrapper(*args: Any, **kwargs: Any) -> R:
+        logger.info("Executing function: %s", func.__name__)
+        return func(*args, **kwargs)
+    return wrapper
